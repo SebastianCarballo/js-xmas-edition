09b14979ba299f8de2ab99430ea646ce13cb07d1
SebastianCarballo <sebastiancarballo.sc.sc@gmail.com>
2022-11-13 13:37:31 -0300

    Actualiza Repositorio

--- tarea/tarea-clase-8.js
+++ tarea/tarea-clase-8.js
@@ -1,7 +1,7 @@
 /*# Tarea clase 8
 
 A las 2 tareas de la clase 6, ponerles las validaciones que consideren
-necesarias.
+necesarias (usando RegExp, Objetos, forEach,poner estilos,mostrar errores en la interfaz de usuario, y escribir pruebas)
 
 TIP: Las edades no pueden tener decimales.
 */
